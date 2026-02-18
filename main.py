3632712f286a3857c1a9acf071be5af921462e76
kwabenamante39 <kwabenamante39@gmail.com>
2026-02-18 22:38:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
 from fastapi import FastAPI
-import requests
 import pandas as pd
-from io import StringIO
 
 app = FastAPI()
 
-DATA_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_2023.csv"
+# Load datasets
+atp_data = pd.read_csv("atp_matches_2023.csv")
+wta_data = pd.read_csv("wta_matches_2023.csv")
 
 @app.get("/")
 def home():
-    return {"message": "Tennis Backend Running"}
+    return {"message": "Advanced Tennis Prediction Backend Running"}
 
 @app.get("/predict")
-def predict(player1_rank: int, player2_rank: int):
+def predict(tour: str, player1: str, player2: str):
 
-    score1 = 100 - player1_rank
-    score2 = 100 - player2_rank
+    if tour.lower() == "atp":
+        data = atp_data
+    else:
+        data = wta_data
 
-    total = score1 + score2
+    # Recent form (last 10 matches)
+    p1_matches = data[(data["winner_name"] == player1) | (data["loser_name"] == player1)].tail(10)
+    p2_matches = data[(data["winner_name"] == player2) | (data["loser_name"] == player2)].tail(10)
 
-    prob1 = (score1 / total) * 100
-    prob2 = (score2 / total) * 100
+    p1_wins = len(p1_matches[p1_matches["winner_name"] == player1])
+    p2_wins = len(p2_matches[p2_matches["winner_name"] == player2])
 
-    winner = "Player 1" if prob1 > prob2 else "Player 2"
+    # Head-to-head
+    h2h = data[
+        ((data["winner_name"] == player1) & (data["loser_name"] == player2)) |
+        ((data["winner_name"] == player2) & (data["loser_name"] == player1))
+    ]
+
+    p1_h2h = len(h2h[h2h["winner_name"] == player1])
+    p2_h2h = len(h2h[h2h["winner_name"] == player2])
+
+    # Weighted scoring
+    p1_score = (p1_wins * 2) + (p1_h2h * 3)
+    p2_score = (p2_wins * 2) + (p2_h2h * 3)
+
+    total = p1_score + p2_score
+
+    if total == 0:
+        return {"error": "Not enough data"}
+
+    p1_prob = (p1_score / total) * 100
+    p2_prob = (p2_score / total) * 100
+
+    winner = player1 if p1_prob > p2_prob else player2
 
     return {
-        "player1_probability": round(prob1, 2),
-        "player2_probability": round(prob2, 2),
+        "player1_probability": round(p1_prob, 2),
+        "player2_probability": round(p2_prob, 2),
         "predicted_winner": winner
     }
-
-@app.get("/top_players")
-def top_players():
-    try:
-        response = requests.get(DATA_URL)
-        df = pd.read_csv(StringIO(response.text))
-
-        top = df[['winner_name','winner_rank']].dropna().sort_values('winner_rank').head(10)
-
-        return top.to_dict(orient="records")
-
-    except:
-        return {"error": "Data fetch failed"}
-#
